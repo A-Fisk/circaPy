6c17d9c13a5040feb8bb6ad3c768d975e5e00ab7
afawole <ayobami.a.fawole@gmail.com>
2026-06-08 11:55:42 +0100

    copied file from original repo

--- circaPy/plots.py
+++ circaPy/plots.py
@@ -9,7 +9,6 @@ import circaPy.activity as act
 import circaPy.preprocessing as prep
 
 
-
 @prep.validate_input
 @prep.invert_light_values
 @prep.plot_kwarg_decorator
@@ -88,7 +87,7 @@ def plot_actogram(
 
     # add entire day of 0s at start and end by extending index
     # grab values from current index
-    freq = pd.infer_freq(data_plot.index)
+    freq = data_plot.index.freq # Decorator enforces
     start = data_plot.index.min()
     end = data_plot.index.max()
 
@@ -115,13 +114,21 @@ def plot_actogram(
     # create new index and set data to it
     extended_index = pd.date_range(start=extended_start, end=extended_end, freq=freq)
     data_plot = data_plot.reindex(extended_index, fill_value=-100)
+    data_light = data_light.reindex(extended_index, fill_value = -100)
 
     # select just the days
     days = data_plot.index.normalize().unique()
 
+    # Extract numpy values
+    data_values = data_plot.values
+    light_values = data_light.values
+
     # set all 0 values to be very low so not showing on y index starting at 0
-    for mask in data_plot, data_light:
-        mask[mask == 0] = -100
+    data_values[data_values == 0] = -100
+    light_values[light_values == 0] = -100
+
+    # Set interactive off to speed up plotting
+    plt.ioff()
 
     # Create figure and subplot for every day
     # create a new figure if not passed one when called
@@ -136,47 +143,42 @@ def plot_actogram(
 
         # draw subplots for each day on the subplot given
         subplot_spec = subplot.get_subplotspec()
-        subplot_grid = gs.GridSpecFromSubplotSpec(
+        subfig = fig.add_subfigure(subplot_spec)
+        ax = subfig.subplots(
             nrows=(len(days) - 1),
             ncols=1,
-            subplot_spec=subplot_spec,
-            wspace=0,
-            hspace=0,
+            gridspec_kw={"wspace":0, "hspace":0}
         )
-        ax = []
-        for grid in subplot_grid:
-            sub_ax = plt.Subplot(fig, grid)
-            fig.add_subplot(sub_ax)
-            ax.append(sub_ax)
+
+    # Fill missing/0s with nans to avoid horizontal lines
+    fill_data_full = np.where(data_values > 0, data_values, np.nan)
+    fill_ldr_full = np.where(light_values > 0, light_values, np.nan)
+    index_arr = data_plot.index
+
+    # Create list of starts/ends to speed up loop
+    day_starts = index_arr.searchsorted(days)
+    day_ends = index_arr.searchsorted(days + pd.Timedelta("2d"))
 
     # select each day to then plot on separate axis
     # plot two days on each row
-    for day_label, axis in zip(days, ax):
-        # get two days of data to plot
-        curr_day = str(day_label.date())
-        next_day = str(day_label.date() + pd.Timedelta("1d"))
-        curr_data = data_plot.loc[curr_day:next_day]
-        curr_data_light = data_light.loc[curr_day:next_day]
-
-        # create masked data for fill between to avoid horizontal lines
-        fill_data = curr_data.where(curr_data > 0)
-        fill_ldr = curr_data_light.where(curr_data_light > 0)
+    for start, end, axis in zip(day_starts, day_ends, ax):
+        # Find start and end of data to plot
+        curr_index = index_arr[start:end]
 
         # plot the data and light_col
-        axis.fill_between(fill_ldr.index, fill_ldr, alpha=ldralpha, facecolor="grey")
-        axis.plot(curr_data, linewidth=linewidth)
-        axis.fill_between(fill_data.index, fill_data)
+        axis.plot(curr_index, data_values[start:end], linewidth=linewidth)
+        axis.fill_between(curr_index, fill_data_full[start:end])
+        axis.fill_between(curr_index, fill_ldr_full[start:end], alpha=ldralpha, facecolor="grey")
 
-        # need to hide all the axis to make visible
+        # need to hide all the axis to make data visible
         axis.set(
             xticks=[],
-            xlim=[curr_data.index[0], curr_data.index[-1]],
+            xlim=[curr_index[0], curr_index[-1]],
             yticks=[],
             ylim=ylim,
         )
-        spines = ["left", "right", "top", "bottom"]
-        for pos in spines:
-            axis.spines[pos].set_visible(False)
+        for spine in axis.spines.values():
+            spine.set_visible(False)
 
     # create the y labels for every 10th row
     day_markers = np.arange(0, len(days), 10)
@@ -200,20 +202,25 @@ def plot_actogram(
     if "timeaxis" in kwargs:
         params_dict["timeaxis"] = kwargs["timeaxis"]
 
+    # Restore interactive
+    plt.ion()
+
     return fig, ax, params_dict
 
 
 @prep.validate_input
 @prep.invert_light_values
 @prep.plot_kwarg_decorator
-def plot_activity_profile(data,
-                          col=[0],
-                          light_col=-1,
-                          subplot=None,
-                          resample=False,
-                          resample_freq="h",
-                          *args,
-                          **kwargs):
+def plot_activity_profile(
+    data,
+    col=0,
+    light_col=-1,
+    subplot=None,
+    resample=False,
+    resample_freq="h",
+    *args,
+    **kwargs,
+):
     """
     Plot the activity profile with mean and SEM (Standard Error of the Mean).
     Optionally resample the data before plotting.
@@ -257,26 +264,28 @@ def plot_activity_profile(data,
     if resample:
         data = data.resample(resample_freq).mean()
 
-    # Select LDR column and perform light calculations                        
+    # Makes col a list to works with looping
+    col = [col] if isinstance(col, int) else col
+
+    # Select LDR column and perform light calculations
     light_data = data.iloc[:, light_col]
     light_mean = act.calculate_mean_activity(light_data)
 
     # Convert the index of mean and sem to a DatetimeIndex starting 2001-01-01
-    start_date = "2001-01-01"
-    freq = pd.infer_freq(data.index)
-    datetime_index = pd.date_range(
-        start=start_date, periods=len(mean), freq=freq)
+    datetime_index, freq = _create_datetime_index(data, light_mean)
     light_mean.index = datetime_index
 
     # Ensure freq has a numeric component
     if not any(char.isdigit() for char in freq):
         freq = pd.Timedelta("1" + freq)  # Prepend '1' if missing
-    # Extend the light_mean data by one extra period and forward fill
+
+    # Extend the light_mean data to fill to the end of the plot
     light_mean = pd.concat(
         [
             light_mean,
             pd.Series(
-                [light_mean.iloc[-1]], index=[light_mean.index[-1] + pd.Timedelta(freq)]
+                [light_mean.iloc[-1]],
+                index=[light_mean.index[-1] + pd.Timedelta(freq)]
             ),
         ]
     )
@@ -284,47 +293,38 @@ def plot_activity_profile(data,
 
     # Offset the mean and sem data to plot in the middle of the hour
     offset_time = 0.5 * pd.Timedelta(freq)
-    mean.index += offset_time
-    sem.index += offset_time
     light_mean.index += offset_time
 
     # Set up column selection and plotting
     num_cols = len(col)
-    fig, axes = plt.subplots(nrows=num_cols, ncols=1, figsize=(10, 4 * num_cols), sharex=True)
+    fig, axes = plt.subplots(
+        nrows=num_cols, ncols=1, figsize=(10, 4 * num_cols), sharex=True
+    )
 
     # Plotting in case just 1 PIR col is selected
     if num_cols == 1:
         axes = [axes]
-      
+
     # Loop to plot multiple PIRs if needed
-    for i, col_idx in enumerate(col):
-        ax = axes[i]
+    for ax, curr_col in zip(axes, col):
         # Select the PIR Columns you want to plot as indecies (PIR1 = 0, PIR2 = 1, etc.)
-        curr_data = data.iloc[:, col_idx]
-        col_name = data.columns[col_idx]
+        col_name = data.columns[curr_col]
+        curr_data = data.loc[:, col_name]
 
         # Calculate mean activity and SEM for PIR data
         mean, sem = act.calculate_mean_activity(curr_data, sem=True)
-        mean.index = datetime_index
+        mean.index = datetime_index 
         sem.index = datetime_index
-
-        # Offset the mean and sem data to plot in the middle of the hour
-        offset_time = 0.5 * pd.Timedelta(freq)
         mean.index += offset_time
         sem.index += offset_time
-        light_mean.index += offset_time
 
         # Plot mean line
         ax.plot(mean.index, mean, label=f"{col_name}", color="blue", linewidth=2)
 
         # Plot SEM shading
         ax.fill_between(
-        mean.index,
-        mean - sem,
-        mean + sem,
-        color="blue",
-        alpha=0.3,
-        label="± SEM")
+            mean.index, mean - sem, mean + sem, color="blue", alpha=0.3, label="± SEM"
+        )
 
         xlim = [mean.index[0], (mean.index[0] + pd.Timedelta("24h"))]
 
@@ -342,22 +342,19 @@ def plot_activity_profile(data,
         # The formula to scale the values is:
         # scaled_value = (value - min_value) / (max_value - min_value)
         # * (target_max - target_min) + target_min
-        scaled_light_mean = (light_mean - min_light_mean
-                         ) / (max_light_mean - min_light_mean
-                              ) * (target_max - target_min) + target_min
-        
+        scaled_light_mean = (light_mean - min_light_mean) / (
+            max_light_mean - min_light_mean
+        ) * (target_max - target_min) + target_min
+
         # Add lights region
         ax.fill_between(
-            scaled_light_mean.index,
-            scaled_light_mean,
-            color='grey',
-            alpha=0.2
+            scaled_light_mean.index, scaled_light_mean, color="grey", alpha=0.2
         )
 
         ax.set_xlabel("Time")
-        #ax.set_ylabel("Activity")
+        # ax.set_ylabel("Activity")
         ax.set_title(f"Activity Profile with Mean and SEM: {col_name}")
-        ax.set_ylim([0, ylim[1]]) # Auto-scale max, but start at 0
+        ax.set_ylim([0, ylim[1]])  # Auto-scale max, but start at 0
         ax.legend()
 
     # create defaults dict
@@ -375,3 +372,10 @@ def plot_activity_profile(data,
         params_dict["timeaxis"] = kwargs["timeaxis"]
 
     return fig, ax, params_dict
+
+def _create_datetime_index(data, mean, start_date="2001-01-01"):
+
+    freq = pd.infer_freq(data.index)
+    datetime_index = pd.date_range(start=start_date, periods=len(mean), freq=freq)
+
+    return datetime_index, freq
